9e393f2e7895570e2fef953790a6d35445142d8c
Ravindu Dinushan <ravindudinushan9966@gmail.com>
2024-07-11 17:18:38 +0530

    first commit

---
+++ chatgpt/constants/Colors.ts
@@ -0,0 +1,18 @@
+export default {
+  primary: '#20AB6E',
+  lime: '#D7FFD4',
+  pink: '#F655FF',
+  brown: '#29271D',
+  sky: '#E5EDFF',
+  teal: '#0E4D45',
+  yellow: '#FCBB80',
+  orange: '#EF580B',
+  blue: '#0000FA',
+  green: '#172E15',
+  light: '#FFFCFF',
+  grey: '#242026',
+  greyLight: '#B8B3BA',
+  input: '#EEE9F0',
+  selected: '#F7F2F9',
+  dark: '#2F2D32',
+};
